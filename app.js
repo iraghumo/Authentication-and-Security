6b6099d218ce46aaf7b6b89cbc5da2747f7a9e89
Raghu Mo <35867347+iraghumo@users.noreply.github.com>
2020-05-23 22:36:37 +0530

    Hashing & Salting

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const md5 = require('md5');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 const app = express();
 
@@ -38,29 +40,34 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
     }else if(foundUser){
-      if(foundUser.password === password){
-        res.render("secrets");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, response) {
+        // res === true
+        if(response == true){
+            res.render("secrets");
+        }
+      });
     }
   });
 });
