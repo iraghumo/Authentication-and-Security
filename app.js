4351037ff194839e023aa6d64648a88036c806fa
Raghu Mo <35867347+iraghumo@users.noreply.github.com>
2020-05-23 19:58:43 +0530

    DB Encryption using mongoose-encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -18,6 +19,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+const secretKey = "skyisblueduetoozone.";
+userSchema.plugin(encrypt, {secret: secretKey, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
